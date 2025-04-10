ab12ea19b9b6b3415273d7fb9ee19386577ad9a8
dpes44 <dpes.adikari44@gmail.com>
2025-04-10 14:16:23 +0545

    removed referenced and orginial conflict

--- teachers/global/global.js
+++ teachers/global/global.js
@@ -1,31 +1,31 @@
 
-let showContentIfOriginal = document.querySelectorAll(".showContentIfOriginal");
-let showContentIfReferenced = document.querySelectorAll(".showContentIfReferenced");
+// let showContentIfOriginal = document.querySelectorAll(".showContentIfOriginal");
+// let showContentIfReferenced = document.querySelectorAll(".showContentIfReferenced");
 
-function showIfOriginal() {
-    showContentIfOriginal.forEach((content) => {
-        content.classList.remove("hidden");
-    })
+// function showIfOriginal() {
+//     showContentIfOriginal.forEach((content) => {
+//         content.classList.remove("hidden");
+//     })
 
-    showContentIfReferenced.forEach((content) => {
-        content.classList.add("hidden");
-    })
-}
+//     showContentIfReferenced.forEach((content) => {
+//         content.classList.add("hidden");
+//     })
+// }
 
-function showIfReferenced() {
-    showContentIfOriginal.forEach((content) => {
-        content.classList.add("hidden");
-    })
+// function showIfReferenced() {
+//     showContentIfOriginal.forEach((content) => {
+//         content.classList.add("hidden");
+//     })
 
-    showContentIfReferenced.forEach((content) => {
-        content.classList.remove("hidden");
-    })
-}
+//     showContentIfReferenced.forEach((content) => {
+//         content.classList.remove("hidden");
+//     })
+// }
 
-document.addEventListener("DOMContentLoaded", function () {
-    if (contentOrigin === "Original" || contentOrigin === "") {
-        showIfOriginal();
-    } else {
-        showIfReferenced();
-    }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     if (contentOrigin === "Original" || contentOrigin === "") {
+//         showIfOriginal();
+//     } else {
+//         showIfReferenced();
+//     }
+// });
