ea51729d05b2edc6367231271ee4651c32b32ad2
Uttam Pradhan <uttampra1@gmail.com>
2025-04-10 17:11:05 +0545

    refactor content display logic: move original and referenced item handling to checkCOntentOrigin.js

--- student/global/studentGlobal.js
+++ student/global/studentGlobal.js
@@ -45,34 +45,3 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error("GraphQL fetch error:", error);
   }
 });
-
-let showContentIfOriginal = document.querySelectorAll(".showContentIfOriginal");
-let showContentIfReferenced = document.querySelectorAll(".showContentIfReferenced");
-
-function showIfOriginal() {
-  showContentIfOriginal.forEach((content) => {
-    content.classList.remove("hidden");
-  })
-
-  showContentIfReferenced.forEach((content) => {
-    content.classList.add("hidden");
-  })
-}
-
-function showIfReferenced() {
-  showContentIfOriginal.forEach((content) => {
-    content.classList.add("hidden");
-  })
-
-  showContentIfReferenced.forEach((content) => {
-    content.classList.remove("hidden");
-  })
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  if (contentOrigin === "Original" || contentOrigin === "") {
-    showIfOriginal();
-  } else {
-    showIfReferenced();
-  }
-});
