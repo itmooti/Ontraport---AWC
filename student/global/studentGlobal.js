2c89bf6e1418f1d9065035887b8a288fd53eb660
Uttam Pradhan <uttampra1@gmail.com>
2025-04-10 17:04:07 +0545

    refactor content display logic for original and referenced items in studentGlobal.js; remove redundant functions from moduleModal.js

--- student/global/studentGlobal.js
+++ student/global/studentGlobal.js
@@ -46,6 +46,33 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
+let showContentIfOriginal = document.querySelectorAll(".showContentIfOriginal");
+let showContentIfReferenced = document.querySelectorAll(".showContentIfReferenced");
 
+function showIfOriginal() {
+  showContentIfOriginal.forEach((content) => {
+    content.classList.remove("hidden");
+  })
 
-  
+  showContentIfReferenced.forEach((content) => {
+    content.classList.add("hidden");
+  })
+}
+
+function showIfReferenced() {
+  showContentIfOriginal.forEach((content) => {
+    content.classList.add("hidden");
+  })
+
+  showContentIfReferenced.forEach((content) => {
+    content.classList.remove("hidden");
+  })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (contentOrigin === "Original" || contentOrigin === "") {
+    showIfOriginal();
+  } else {
+    showIfReferenced();
+  }
+});
