9dd2808a6851b46fcd08336cf9774f9c6cb40567
Uttam Pradhan <uttampra1@gmail.com>
2025-04-11 10:20:14 +0545

    add global.js and global.css: implement notification button toggle functionality with linked states

---
+++ admin/notification/global/global.js
@@ -0,0 +1,60 @@
+const buttonLinks = new Map();
+
+function toggleButton(button, isSliderOff) {
+    const onOffBtn = button.querySelector(".onOffTxt");
+    const children = Array.from(button.children);
+
+    button.innerHTML = "";
+    children.reverse().forEach((child) => button.appendChild(child));
+
+    if (isSliderOff) {
+        button.style.setProperty("background-color", "#007c8f", "important");
+        onOffBtn.innerText = "ON";
+        onOffBtn.style.setProperty("color", "#fff", "important");
+    } else {
+        button.style.setProperty("background-color", "#bfbfbf", "important");
+        onOffBtn.innerText = "OFF";
+        onOffBtn.style.setProperty("color", "#fff", "important");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".notificationButton");
+
+    const buttonStates = new Map();
+
+    buttons.forEach((button, index) => {
+        buttonStates.set(button, true);
+        button.dataset.index = index;
+
+        button.addEventListener("click", (e) => {
+            e.stopPropagation();
+
+            const isSliderOff = buttonStates.get(button);
+            toggleButton(button, isSliderOff);
+            buttonStates.set(button, !isSliderOff);
+
+            if (buttonLinks.has(button.dataset.index)) {
+                const linkedButtons = buttonLinks.get(button.dataset.index);
+                linkedButtons.forEach((linkedIndex) => {
+                    const linkedButton = Array.from(buttons).find(
+                        (btn) => btn.dataset.index === linkedIndex
+                    );
+                    if (linkedButton && buttonStates.get(linkedButton) === isSliderOff) {
+                        toggleButton(linkedButton, isSliderOff);
+                        buttonStates.set(linkedButton, !isSliderOff);
+                    }
+                });
+            }
+        });
+    });
+
+    const linkButtons = (btnIndex1, btnIndex2) => {
+        if (!buttonLinks.has(btnIndex1)) buttonLinks.set(btnIndex1, []);
+        if (!buttonLinks.has(btnIndex2)) buttonLinks.set(btnIndex2, []);
+        buttonLinks.get(btnIndex1).push(btnIndex2);
+        buttonLinks.get(btnIndex2).push(btnIndex1);
+    };
+
+    linkButtons("1", "2");
+});
