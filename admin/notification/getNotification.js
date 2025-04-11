849e7dbcf7675d14919eeb068372ae6ad2cffc0d
Uttam Pradhan <uttampra1@gmail.com>
2025-04-11 11:38:50 +0545

    fix: update notification redirection URLs for admin and student views

--- admin/notification/getNotification.js
+++ admin/notification/getNotification.js
@@ -316,11 +316,11 @@ function createNotificationCard(notification, isRead) {
             await markAsRead(id);
         }
         if (type === "Posts" || type === "Post Comments") {
-            window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=courseChat?current-post-id=${notification.Post_ID}`;
+            window.location.href = `https://courses.writerscentre.com.au/admin/class/${notification.Course_Unique_ID}?selectedTab=chats?current-post-id=${notification.Post_ID}`;
         } else if (type === "Submissions" || type === "Submission Comments") {
-            window.location.href = `https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID1}?eid=${notification.EnrolmentID}`;
+            window.location.href = `https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID1}`;
         } else {
-            window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`;
+            window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?&selectedTab=announcements?data-announcement-template-id=${anouncementScrollId}`;
         }
     });
     return card;
