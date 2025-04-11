42aed6982c146fe27ed60e6b70f5d0af7b9d3e67
Uttam Pradhan <uttampra1@gmail.com>
2025-04-11 14:11:17 +0545

    refactor notification links to use enrollID instead of URL parameters

--- student/global/studentNotificationsGlobal.js
+++ student/global/studentNotificationsGlobal.js
@@ -13,9 +13,6 @@ if (dates.length > 0) {
   window.earliestEnrollmentDate = createdAt;
 }
 
-function getEidasAStudentPortal() {
-  return new URLSearchParams(window.location.search).get("eid");
-}
 
 //Set user preferences
 const POSTS_TYPE =
@@ -636,7 +633,6 @@ function createNotificationCard(notification, isRead) {
     const id = Number(notification.ID);
     const type = notification.Notification_Type;
     const loader = document.getElementById("loader");
-    const getEidasAStudentPortal = getEidasAStudentPortal();
     const anouncementScrollId =
       String(notification.Notification_Type) !== "Announcements"
         ? notification.ForumComments_Parent_Announcement_ID
@@ -646,11 +642,11 @@ function createNotificationCard(notification, isRead) {
       await markAsRead(id);
     }
     if (type === "Posts" || type === "Post Comments") {
-      window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${getEidasAStudentPortal}&selectedTab=courseChat?current-post-id=${notification.Post_ID}`;
+      window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${enrollID}&selectedTab=courseChat?current-post-id=${notification.Post_ID}`;
     } else if (type === "Submissions" || type === "Submission Comments") {
-      window.location.href = `https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID1}?eid=${getEidasAStudentPortal}`;
+      window.location.href = `https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID1}?eid=${enrollID}`;
     } else {
-      window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${getEidasAStudentPortal}&selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`;
+      window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${enrollID}&selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`;
     }
   });
   return card;
