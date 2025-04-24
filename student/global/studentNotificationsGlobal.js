bae8f59db30c59c2e4ab1e18c9ab90b3101590eb
Andrew Wadsworth <andrew@itmooti.com>
2025-04-24 17:13:47 +0545

    Update studentNotificationsGlobal.js

--- student/global/studentNotificationsGlobal.js
+++ student/global/studentNotificationsGlobal.js
@@ -277,7 +277,6 @@ function getQueryParamss(param) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
 }
-
 const enrollID = getQueryParamss("eid");
 
 function timeAgo(unixTimestamp) {
@@ -643,31 +642,21 @@ function createNotificationCard(notification, isRead) {
   card.addEventListener("click", async function () {
     const id = Number(notification.ID);
     const type = notification.Notification_Type;
-    
-    // const loader = document.getElementById("loader");
-
-    const enrolmentID = notification.EnrolmentID;
-    console.log("Eid from notifications is", enrolmentID);
-    
+    const loader = document.getElementById("loader");
     const anouncementScrollId =
       String(notification.Notification_Type) !== "Announcements"
         ? notification.ForumComments_Parent_Announcement_ID
         : notification.ID;
-    
-        // loader.classList.remove("fade-out");
-    
-        if (!readAnnouncements.has(id) && !pendingAnnouncements.has(id)) {
+    loader.classList.remove("fade-out");
+    if (!readAnnouncements.has(id) && !pendingAnnouncements.has(id)) {
       await markAsRead(id);
     }
     if (type === "Posts" || type === "Post Comments") {
-        console.log(`https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=courseChat?current-post-id=${notification.Post_ID}`);
-        // window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=courseChat?current-post-id=${notification.Post_ID}`;
+      window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=courseChat?current-post-id=${notification.Post_ID}`;
     } else if (type === "Submissions" || type === "Submission Comments") {
-    //   window.location.href = `https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID5}?eid=${notification.EnrolmentID}`;
-      console.log(`https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID5}?eid=${notification.EnrolmentID}`);
-    } else {
-    //   window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`;
-      console.log(`https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`);
+      window.location.href = `https://courses.writerscentre.com.au/course-details/content/${notification.Lesson_Unique_ID5}?eid=${notification.EnrolmentID}`;
+    } else {notification.Enrolment_ID
+      window.location.href = `https://courses.writerscentre.com.au/students/course-details/${notification.Course_Unique_ID}?eid=${notification.EnrolmentID}&selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`;
     }
   });
   return card;
