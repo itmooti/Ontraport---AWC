fecb93417af90b3ee5119ac9ca200295d5b3817d
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2025-04-25 10:21:35 +0545

    Update studentNotificationsGlobal.js

--- student/global/studentNotificationsGlobal.js
+++ student/global/studentNotificationsGlobal.js
@@ -725,9 +725,7 @@ function createNotificationCard(notification, isRead) {
           ${message}
         </div>
         <div class="extra-small-text text-dark line-clamp-2">${messageContent}</div>
-        <div class="text-[#586A80] extra-small-text">${
-          notification.Course_Course_Name || "Course"
-        }</div>
+        <div class="text-[#586A80] extra-small-text">${notification_course_name}</div>
       </div>
       <div class="extra-small-text text-[#586A80] text-nowrap">${timeAgo(
         notification.Date_Added
