7ea5b1027884f3174e80e3d1153cc72b7bd359b2
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2025-05-05 15:47:55 +0545

    Update courseContent.js

--- student/classRoom/courseContent.js
+++ student/classRoom/courseContent.js
@@ -381,7 +381,7 @@ Lessons(
   ClassCustomisations(
     query: [
       { where: { type: "Assessment" } 
-      { andWhere: {class_to_modify_id: ${classID}} }}
+              { andWhere: { class_to_modify_id: ${classID} } } 
     ]
     limit: 1
     offset: 0
