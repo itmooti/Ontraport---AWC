eb262065568e1f4c9fb65815771c2e6b55cc2b00
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2025-05-26 16:17:20 +0545

    Update getNotification.js

--- teachers/classRoom/getNotification.js
+++ teachers/classRoom/getNotification.js
@@ -392,7 +392,7 @@ function createNotificationCard(notification, isRead) {
 
     if ((type === "Posts" || type === "Post Comments") && notification.Post_ID) {
     //  const myEidFromCourse = await getEnrolmentIdsByCourseUid(courseUid, activeOrInactive);
-      window.location.href = `https://courses.writerscentre.com.au/teacher/class/${classIDForADmin}?selectedTab=courseChat?current-post-id=${notification.Post_ID}`;
+      window.location.href = `https://courses.writerscentre.com.au/teacher/class/${classIDForADmin}?selectedTab=chats?current-post-id=${notification.Post_ID}`;
     } else if ((type === "Submissions" || type === "Submission Comments") && notification.Submissions?.Assessment?.Lesson?.unique_id) {
       const lessonUid = notification.Submissions.Assessment.Lesson.unique_id;
      // const myEidFromLesson = await getEnrolmentIdsByLessonUid(lessonUid, activeOrInactive);
@@ -400,7 +400,7 @@ function createNotificationCard(notification, isRead) {
           window.location.href = `https://courses.writerscentre.com.au/course-details/content/${lessonUid}?submissionPostIs=${notification.Submissions_ID}${assessmentType === "File Submission" ? `&subUID=${subUID}&commentScrollId=${commentScrollID}` : ""}`
     } else {
      // const myEidFromCourse = await getEnrolmentIdsByCourseUid(courseUid, activeOrInactive);
-      window.location.href = `https://courses.writerscentre.com.au/teacher/class/${classIDForADmin}?selectedTab=anouncemnt?data-announcement-template-id=${anouncementScrollId}`;
+      window.location.href = `https://courses.writerscentre.com.au/teacher/class/${classIDForADmin}?selectedTab=announcements?data-announcement-template-id=${anouncementScrollId}`;
     }
   });
 
