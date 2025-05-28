a56a78b8c989f8b2c8f3fbc3af852c5a07784002
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2025-05-28 10:22:40 +0545

    Update getNotification.js

--- teachers/classRoom/getNotification.js
+++ teachers/classRoom/getNotification.js
@@ -378,7 +378,7 @@ let lowerContent = '';
         </div>
         <div class="extra-small-text text-dark line-clamp-2">${messageContent}</div>
         <div class="text-[#586A80] extra-small-text">${notification_class_name}</div>
-         <div class="text-[#586A80] extra-small-text line-clamp-1">${lowerContent}</div>
+         <div class="text-[#586A80] extra-small-text line-clamp-2">${lowerContent}</div>
       </div>
       <div class="extra-small-text text-[#586A80] text-nowrap">${timeAgo(notification.Date_Added)}</div>
     </div>
