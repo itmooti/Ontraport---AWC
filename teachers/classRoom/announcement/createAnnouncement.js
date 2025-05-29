5363787c8c015be7409a8a67467a46b728909e8b
dpes44 <83898800+dpes44@users.noreply.github.com>
2025-05-29 12:05:38 +0545

    Update createAnnouncement.js

--- teachers/classRoom/announcement/createAnnouncement.js
+++ teachers/classRoom/announcement/createAnnouncement.js
@@ -1,4 +1,4 @@
-
+<script>
 
     const DEFAULT_AVATAR =
         "https://file.ontraport.com/media/41ca85f5cdde4c12bf72c2c73747633f.phpkeya0n?Expires=4884400377&Signature=SnfrlziQIcYSbZ98OrH2guVWpO4BRcxatgk3lM~-mKaAencWy7e1yIuxDT4hQjz0hFn-fJ118InfvymhaoqgGxn63rJXeqJKB4JTkYauub5Jh5UO3z6S0o~RVMj8AMzoiImsvQoPuRK7KnuOAsGiVEmSsMHEiM69IWzi4dW~6pryIMSHQ9lztg1powm8b~iXUNG8gajRaQWxlTiSyhh-CV-7zkF-MCP5hf-3FAKtGEo79TySr5SsZApLjfOo-8W~F8aeXK8BGD3bX6T0U16HsVeu~y9gDCZ1lBbLZFh8ezPL~4gktRbgP59Us8XLyV2EKn6rVcQCsVVUk5tUVnaCJw__&Key-Pair-Id=APKAJVAAMVW6XQYWSTNA";
@@ -238,6 +238,13 @@
             );
         }
 
+        let mentionedIds = [];
+        tempContainer.querySelectorAll(".mention").forEach((mention) => {
+            const id = mention.dataset.contactId;
+            if (id && !mentionedIds.includes(Number(id)))
+                mentionedIds.push(Number(id));
+        });
+        
         const payload = {
             title,
             content,
@@ -460,3 +467,4 @@ mutation updateAnnouncement($id: AwcAnnouncementID, $payload: AnnouncementUpdate
         document.getElementById("allTabs").classList.remove("activeTab");
         fetchAnnouncements();
     });
+</script>
