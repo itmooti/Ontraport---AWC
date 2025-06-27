9ac5a085f57e5bf9739a26a33e5499851b477588
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2025-06-27 10:43:11 +0545

    Update notificationQuery.js

--- admin/notification/notificationQuery.js
+++ admin/notification/notificationQuery.js
@@ -157,6 +157,7 @@ function getSubscriptionQueryForAllClasses() {
         Type: type
         Unique_ID: unique_id
         Class {
+        id 
         unique_id 
           class_name
           Enrolments { id }
