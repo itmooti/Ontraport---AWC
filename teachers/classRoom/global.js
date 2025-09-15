dca231f6c0ec9c2a909851e84a938ead682fe38a
dpes44 <83898800+dpes44@users.noreply.github.com>
2025-09-15 12:40:16 +0545

    Update global.js

--- teachers/classRoom/global.js
+++ teachers/classRoom/global.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
             targetElement.classList.add("highlightTheSelected");
             setTimeout(() => {
                 targetElement.scrollIntoView({ behavior: "smooth", block: "center" });
-            }, 1000);
+            }, 3000);
             return true;
         }
         return false;
